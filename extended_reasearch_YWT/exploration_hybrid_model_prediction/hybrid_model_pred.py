a28bebf947614ef8b569b538382829917f8a4394
YWT <theabc50111@gmail.com>
2022-07-03 16:21:12 +0000

    add exploration_raw_data & exploration_hybrid_model_pred

---
+++ extended_reasearch_YWT/exploration_hybrid_model_prediction/hybrid_model_pred.py
@@ -0,0 +1,218 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+import pandas as pd
+import math
+import os
+import random
+from tqdm import tqdm
+from itertools import combinations
+from sklearn.metrics import mean_squared_error, mean_absolute_error
+from  pmdarima.arima import ARIMA, auto_arima
+from statsmodels.graphics.tsaplots import plot_acf, plot_pacf
+from statsmodels.tsa.seasonal import seasonal_decompose
+from scipy import stats as st
+from sklearn.linear_model import LinearRegression
+from keras.models import Sequential, load_model
+from keras.layers import Dense, LSTM, Activation
+from keras import backend as K
+from keras.utils.generic_utils import get_custom_objects
+from keras.callbacks import ModelCheckpoint
+from keras.regularizers import l1_l2
+import tensorflow.keras as keras
+import warnings
+warnings.simplefilter("ignore")
+
+
+# # Prepare data
+
+
+stock_price_df = pd.read_csv("../../stock08_price.csv")
+
+universe = list(stock_price_df.columns.values[1:])
+universe.remove("SP500")
+# train data
+portfolio_train = ['CELG', 'PXD', 'WAT', 'LH', 'AMGN', 'AOS', 'EFX', 'CRM', 'NEM', 'JNPR', 'LB', 'CTAS', 'MAT', 'MDLZ', 'VLO', 'APH', 'ADM', 'MLM', 'BK', 'NOV', 'BDX', 'RRC', 'IVZ', 'ED', 'SBUX', 'GRMN', 'CI', 'ZION', 'COO', 'TIF', 'RHT', 'FDX', 'LLL', 'GLW', 'GPN', 'IPGP', 'GPC', 'HPQ', 'ADI', 'AMG', 'MTB', 'YUM', 'SYK', 'KMX', 'AME', 'AAP', 'DAL', 'A', 'MON', 'BRK', 'BMY', 'KMB', 'JPM', 'CCI', 'AET', 'DLTR', 'MGM', 'FL', 'HD', 'CLX', 'OKE', 'UPS', 'WMB', 'IFF', 'CMS', 'ARNC', 'VIAB', 'MMC', 'REG', 'ES', 'ITW', 'NDAQ', 'AIZ', 'VRTX', 'CTL', 'QCOM', 'MSI', 'NKTR', 'AMAT', 'BWA', 'ESRX', 'TXT', 'EXR', 'VNO', 'BBT', 'WDC', 'UAL', 'PVH', 'NOC', 'PCAR', 'NSC', 'UAA', 'FFIV', 'PHM', 'LUV', 'HUM', 'SPG', 'SJM', 'ABT', 'CMG', 'ALK', 'ULTA', 'TMK', 'TAP', 'SCG', 'CAT', 'TMO', 'AES', 'MRK', 'RMD', 'MKC', 'WU', 'ACN', 'HIG', 'TEL', 'DE', 'ATVI', 'O', 'UNM', 'VMC', 'ETFC', 'CMA', 'NRG', 'RHI', 'RE', 'FMC', 'MU', 'CB', 'LNT', 'GE', 'CBS', 'ALGN', 'SNA', 'LLY', 'LEN', 'MAA', 'OMC', 'F', 'APA', 'CDNS', 'SLG', 'HP', 'XLNX', 'SHW', 'AFL', 'STT', 'PAYX', 'AIG', 'FOX', 'MA']
+# all data
+portfolio_all = universe
+# all data - train data
+portfolio_other = [p for p in universe if p not in portfolio_train]
+print(len(portfolio_train), len(portfolio_all), len(portfolio_other))
+
+pd.to_datetime(stock_price_df['Date'], format='%Y-%m-%d')
+stock_price_df = stock_price_df.set_index(pd.DatetimeIndex(stock_price_df['Date']))
+# display(stock_price_df)
+
+
+def gen_unseen_data_corr(portfolio: list, ret_date: bool = False) -> "pd.DataFrame, pd.Series | pd.DataFrame":
+    tmp_corr = stock_price_df[portfolio[0]].rolling(window=100).corr(stock_price_df[portfolio[1]])
+    tmp_corr = tmp_corr.iloc[99::100]
+    corr_series = tmp_corr[len(tmp_corr)-21:len(tmp_corr)]
+    # tmp_corr = tmp_corr[:21]
+    unseen_data_df = pd.DataFrame(corr_series).reset_index().drop(['Date'], axis=1).T
+    if ret_date:
+        return unseen_data_df, corr_series
+    else:
+        return unseen_data_df
+
+
+# # ARIMA model
+
+# In[24]:
+
+
+def arima_model(dataset: "pd.DataFrame", portfolio: list, overview: bool = False) -> ("np.array", "pd.DataFrame", str):
+    model_110 = ARIMA(order=(1, 1, 0), out_of_sample_size=0, mle_regression=True, suppress_warnings=True)
+    model_011 = ARIMA(order=(0, 1, 1), out_of_sample_size=0, mle_regression=True, suppress_warnings=True)
+    model_111 = ARIMA(order=(1, 1, 1), out_of_sample_size=0, mle_regression=True, suppress_warnings=True)
+    model_211 = ARIMA(order=(2, 1, 1), out_of_sample_size=0, mle_regression=True, suppress_warnings=True)
+    model_210 = ARIMA(order=(2, 1, 0), out_of_sample_size=0, mle_regression=True, suppress_warnings=True)
+    model_330 = ARIMA(order=(3, 3, 0), out_of_sample_size=0, mle_regression=True, suppress_warnings=True)
+
+    model_dict = {"model_110": model_110, "model_011": model_011, "model_111": model_111, "model_211": model_211, "model_210": model_210}
+
+    tested_models = []
+    arima_model = None
+    find_arima_model = False
+    residual = []
+    for s in np.array(dataset):
+        while not find_arima_model:
+            try:
+                for model_key in model_dict:
+                    if model_key not in tested_models:
+                        test_model = model_dict[model_key].fit(s)
+                        if arima_model is None:
+                            arima_model = test_model
+                            arima_model_name = model_key
+                        elif arima_model.aic() <= test_model.aic():
+                            pass
+                        else:
+                            arima_model = test_model
+                            arima_model_name = model_key
+                    tested_models.append(model_key)
+
+            except Exception:
+                if len(model_dict)-1 != 0:
+                    del model_dict[model_key]
+                else:
+                    print(f"fatal error, {portfolio} doesn't have appropriate arima model")
+                    break
+            else:
+                model_dict = {"model_110": model_110, "model_011": model_011, "model_111": model_111, "model_211": model_211, "model_210": model_210, "model_330": model_330}
+                tested_models.clear()
+                find_arima_model = True
+
+        arima_pred = list(arima_model.predict_in_sample())
+        arima_pred = [np.mean(arima_pred[1:])] + arima_pred[1:]
+        arima_pred = np.clip(np.array(arima_pred), -1, 1)
+
+        res = pd.Series(np.array(s) - arima_pred)
+        residual.append(np.array(res)[:20])
+        find_arima_model = False
+    residual = pd.DataFrame(residual)
+
+    if overview:
+        plt.plot(arima_pred, label="arima_pred")
+        plt.plot(dataset.T, label="data")
+        plt.plot(res, label="res")
+        plt.legend()
+        plt.show()
+        plt.close()
+
+    return arima_pred, residual, arima_model_name
+
+
+# # LSTM
+
+
+class DoubleTanh(Activation):
+    def __init__(self, activation, **kwargs):
+        super(DoubleTanh, self).__init__(activation, **kwargs)
+        self.__name__ = 'double_tanh'
+
+
+def double_tanh(x):
+    return (K.tanh(x) * 2)
+
+
+get_custom_objects().update({'double_tanh': DoubleTanh(double_tanh)})
+
+filepath = './stock_correlation_prediction/models/hybrid_LSTM/epoch262.h5'
+custom_objects = {"Double_Tanh": DoubleTanh}
+with keras.utils.custom_object_scope(custom_objects):
+    lstm_model = load_model(filepath)
+
+
+# # Hybrid model
+
+
+def stl_decompn(corr_series: "pd.Series", overview: bool = False) -> (float, float, float):
+    output_resid = 100000
+    output_trend = None
+    output_period = None
+    for p in range(2, 11):
+        decompose_result_mult = seasonal_decompose(corr_series, period=p)
+        resid_sum = np.abs(decompose_result_mult.resid).mean()
+        if output_resid > resid_sum:
+            output_resid = resid_sum
+            output_trend = decompose_result_mult.trend.dropna()
+            output_period = p
+    
+    reg = LinearRegression().fit(np.arange(len(output_trend)).reshape(-1, 1), output_trend)
+
+    if overview:
+        decompose_result_mult = seasonal_decompose(corr_series, period=output_period)
+        trend = decompose_result_mult.trend.dropna().reset_index(drop=True)
+        plt.figure(figsize=(7, 1))
+        plt.plot(trend)
+        plt.plot([0, len(trend)], [reg.intercept_, reg.intercept_+len(trend)*reg.coef_])
+        plt.title("trend & regression line")
+        plt.show()
+        plt.close()
+        decompose_result_mult.plot()
+        plt.show()
+        plt.close()
+
+    return output_period, output_resid, output_trend.std(), reg.coef_[0]
+
+
+res_list = []
+count = 0
+for portfolio in tqdm(combinations(portfolio_other, 2)):
+    # unseen_data_corr = gen_unseen_data_corr(portfolio)
+    unseen_data_corr_df, unseen_data_corr_series = gen_unseen_data_corr(portfolio, ret_date=True)
+    arima_pred, residual, arima_model_name = arima_model(unseen_data_corr_df, portfolio)
+    unseen_res = residual.values.reshape((-1, 20, 1))
+    lstm_pred = lstm_model.predict(unseen_res)
+    season_period, stl_resid, stl_trend_std, coef_reg_trend = stl_decompn(unseen_data_corr_series)
+    portfolio_res_dic = {"portfolio": f"{portfolio[0]} & {portfolio[1]}",
+                         "corr_ser_mean": unseen_data_corr_series.mean(),
+                         "corr_ser_std": unseen_data_corr_series.std(),
+                         "corr_season_period": season_period,
+                         "corr_stl_resid": stl_resid,
+                         "corr_stl_trend_std": stl_trend_std,
+                         "corr_trend_coef": coef_reg_trend,
+                         "arima_model": arima_model_name,
+                         "lstm_pred": lstm_pred[0][0],
+                         "arima_pred": arima_pred[-1],
+                         "hybrid_model_pred": arima_pred[-1]+lstm_pred[0][0],
+                         "ground_truth": unseen_data_corr_df.iloc[0, -1],
+                         "arima_err": unseen_data_corr_df.iloc[0, -1] - arima_pred[-1],
+                         "lstm_compen_err": arima_pred[-1] - lstm_pred[0][0],
+                         "error": (unseen_data_corr_df.iloc[0, -1] - (arima_pred[-1]+lstm_pred[0][0])),
+                         "absolute_err": math.copysign((unseen_data_corr_df.iloc[0, -1] - (arima_pred[-1]+lstm_pred[0][0])), 1), 
+                         "lstm_compensation_dir": np.sign(unseen_data_corr_df.iloc[0, -1] - arima_pred[-1])*np.sign(lstm_pred[0][0])}
+
+    res_list.append(portfolio_res_dic)
+    #count = count + 1
+    #if count > 10:
+    #    break
+
+
+
+res_df = pd.DataFrame(res_list)
+res_df.to_csv("./stock_correlation_prediction/use_hybrid_model/295stocks_res.csv", index=False)
--- extended_reasearch_YWT/exploration_raw_data/correlation_data_etl.py
+++ extended_reasearch_YWT/exploration_raw_data/correlation_data_etl.py
@@ -1,23 +1,21 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 import statsmodels.api as sm
-import math
-import os
-import random
+
+# # Create Portfolio
+# 
+# out of 505 companies, 150 firms are randomly selected for the portfolio.
 
 
-
-df = pd.read_csv("./stock08_price.csv")
+df = pd.read_csv("../../stock08_price.csv")
 universe = list(df.columns.values[1:])
 universe.remove("SP500")
-print(universe)
-
+# print(universe)
 
 
 # train data
@@ -29,23 +27,26 @@ portfolio_other = [p for p in universe if p not in portfolio_train]
 print(len(portfolio_train), len(portfolio_all), len(portfolio_other))
 
 
-def rolling_corr(item1,item2) :
-    #import data
-    stock_price_df = pd.read_csv("./stock08_price.csv")
+# # Prepare the Data
+
+
+def rolling_corr(item1: str, item2: str) -> "pd.series":
+    # import data
+    stock_price_df = pd.read_csv("../../stock08_price.csv")
     pd.to_datetime(stock_price_df['Date'], format='%Y-%m-%d')
     stock_price_df = stock_price_df.set_index(pd.DatetimeIndex(stock_price_df['Date']))
-    
-    #calculate
+
+    # calculate
     df_pair = pd.concat([stock_price_df[item1], stock_price_df[item2]], axis=1)
     df_corr = df_pair[item1].rolling(window=100).corr(df_pair[item2])
     return df_corr
 
 
-def gen_data(portfolio, file_name="", save_file=False):
+def gen_data(portfolio: list, file_name: str = "", save_file: bool = False) -> "pd.DataFrame":
     index_list = []
     for _ in range(100):
         indices = []
-        for k in range(_, 2420,100):
+        for k in range(_, 2420, 100):
             indices.append(k)
         index_list.append(indices)
 
@@ -55,14 +56,12 @@ def gen_data(portfolio, file_name="", save_file=False):
         for j in range(len(portfolio)-1-i):
             a = portfolio[i]
             b = portfolio[len(portfolio)-1-j]
-            file_name = a + '_' + b
-
             corr_series = rolling_corr(a, b)[99:]
             for _ in range(100):
                 corr_strided = list(corr_series[index_list[_]][:24]).copy()
                 data_matrix.append(corr_strided)
-                count+=1
-                if count % 100000 == 0 :
+                count += 1
+                if count % 100000 == 0:
                     print(str(count)+' items preprocessed')
 
     data_matrix = np.transpose(data_matrix)
@@ -71,14 +70,14 @@ def gen_data(portfolio, file_name="", save_file=False):
         data_dictionary[str(i)] = data_matrix[i]
     data_df = pd.DataFrame(data_dictionary)
     if save_file:
-        data_df.to_csv(f'./stock_correlation_prediction/{file_name}')
-        
+        data_df.to_csv(f'./correlation_record/{file_name}', index=False)
+
     return data_df
 
 
-def gen_corr_series(data_df, file_name="", from_file=False):
+def gen_corr_series(data_df=None, file_name="", from_file=False):
     if from_file:
-        data_df = pd.read_csv(f'./stock_correlation_prediction/{file_name}')
+        data_df = pd.read_csv(f'./correlation_record/{file_name}')
         data_df = data_df.loc[:, ~data_df.columns.str.contains('^Unnamed')]
     ind_range = int(len(data_df)/20)
     num_list = []
@@ -91,12 +90,17 @@ def gen_corr_series(data_df, file_name="", from_file=False):
     return data_df.values.reshape(-1,)
 
 
-train_data_df = gen_data(portfolio_train, "train_dataset.csv", True) 
-all_data_df = gen_data(portfolio_all, "445_dataset.csv", True) 
-other_data_df = gen_data(portfolio_other, "295_dataset.csv", True) 
+if __name__ == "__main__":
+    gen_from_file = True
 
-
-
-
-
-
+    if gen_from_file:
+        train_corr_series = gen_corr_series(None, "train_dataset.csv", True)
+        all_corr_series = gen_corr_series(None, "445_dataset.csv", True)
+        other_corr_series = gen_corr_series(None, "295_dataset.csv", True)
+    else:
+        train_data_df = gen_data(portfolio_train)
+        all_data_df = gen_data(portfolio_all)
+        other_data_df = gen_data(portfolio_other)
+        train_corr_series = gen_corr_series(train_data_df)
+        all_corr_series = gen_corr_series(all_data_df)
+        other_corr_series = gen_corr_series(other_data_df)
