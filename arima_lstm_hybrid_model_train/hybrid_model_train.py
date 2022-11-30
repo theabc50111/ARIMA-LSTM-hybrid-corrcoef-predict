29ab81c2f483a107c5ebb5699486333a4d616928
YWT <theabc50111@gmail.com>
2022-11-30 14:53:40 +0000

    retrain sp500_20082017_corr_ser_abs_hrchy_12

--- arima_lstm_hybrid_model_train/hybrid_model_train.py
+++ arima_lstm_hybrid_model_train/hybrid_model_train.py
@@ -55,11 +55,11 @@ logging.debug(pformat(data_cfg, indent=1, width=100, compact=True))
 save_corr_data = True
 save_arima_resid_data = True
 # data implement setting
-data_implement = "SP500_20082017_CORR_SER_ABS_HRCHY_12_CLUSTER"  # watch options by operate: print(data_cfg["DATASETS"].keys())
+data_implement = "SP500_20082017"  # watch options by operate: print(data_cfg["DATASETS"].keys())
 # train set setting
 train_items_setting = "-train_train"  # -train_train|-train_all
 # data split  period setting, only suit for only settings of Korean paper
-data_split_settings = ["-data_sp_train", "-data_sp_dev", "-data_sp_test1", "-data_sp_test2", ]
+data_split_settings = ["-data_sp_train", "-data_sp_dev", "-data_sp_test1", "-data_sp_test2"]
 # lstm_hyper_params
 lstm_hyper_param = "-kS_hyper"
 
